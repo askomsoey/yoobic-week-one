dc9fca650ad6534569de7a07e1eb4d9dc9d2abf3
askomsoey-yoobic <askomsoey@yoobic.com>
2022-02-04 17:24:23 +0100

    fix(ana-header): change unnecessarily long variable names

--- design-system/src/components/organisms/ana-header/ana-header.tsx
+++ design-system/src/components/organisms/ana-header/ana-header.tsx
@@ -8,21 +8,21 @@ import { Tab } from '../../shared/tab';
   shadow: true,
 })
 export class AnaHeader implements ComponentInterface {
-  @Prop() headerBackgroundImg?: string;
-  @Prop() pageLogoSrc!: string;
-  @Prop() titleContent!: string;
-  @Prop() subtitleContent?: string;
-  @Prop() tabsData: Tab[] = [];
+  @Prop() backgroundImg?: string;
+  @Prop() logoSrc!: string;
+  @Prop() title!: string;
+  @Prop() subtitle?: string;
+  @Prop() tabs: Tab[] = [];
 
   render() {
     return (
       <Host>
-        <div class="header-img-container">{this.headerBackgroundImg && <img src={this.headerBackgroundImg} alt="header" />}</div>
+        <div class="header-img-container">{this.backgroundImg && <img src={this.backgroundImg} alt="header" />}</div>
         <div class="control-panel">
-          <ana-image src={this.pageLogoSrc} alt="header logo" size="medium"></ana-image>
-          <ana-title content={this.titleContent} subtitle={this.subtitleContent || undefined} size="medium" color="#fff"></ana-title>
+          <ana-image src={this.logoSrc} alt="header logo" size="medium"></ana-image>
+          <ana-title content={this.title} subtitle={this.subtitle || undefined} size="medium" color="#fff"></ana-title>
           <ana-button content="Join" identifier="joinButton"></ana-button>
-          <ana-tabs tabs={this.tabsData} buttonType="tab" identifier="headerTabs"></ana-tabs>
+          <ana-tabs tabs={this.tabs} buttonType="tab" identifier="headerTabs"></ana-tabs>
         </div>
       </Host>
     );
